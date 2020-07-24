530559e6e1824518f33e7deae66f39c09a657b78
Rinat Nadav <ringhost@gmail.com>
2020-07-24 19:22:42 +0300

    added onclick function to Accordion component

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 
 const Accordion = ({items}) =>{
-    const renderedItems = items.map(item=>{
+    const onTitleClick = (index) =>{
+        console.log(index);
+    }
+    const renderedItems = items.map((item, index)=>{
         return(
             < React.Fragment key={item.title}>
-                <div className="title active">
+                <div 
+                className="title active"
+                //we cant use onclick={onTitleClick} so it wont envoked the moment the component is rendered
+                 onClick = {()=> onTitleClick(index)}
+                 >
                     <i className="dropdown icon"></i>
                     {item.title}
                 </div>
