961f391c5284e683683a1437c56ebf48a7f2785a
Rinat Nadav <ringhost@gmail.com>
2020-07-25 22:08:35 +0300

    added a dropdown onclick function

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -1,21 +1,28 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 const Accordion = ({items}) =>{
+    // state={
+    //     activeIndex:0,
+    // }
+    const [activeIndex, setActiveIndex] = useState(null)
     const onTitleClick = (index) =>{
-        console.log(index);
+        // this.setState({activeIndex:index})
+        setActiveIndex(index)
     }
     const renderedItems = items.map((item, index)=>{
+        const active = index === activeIndex ? 'active' : '';
+        
         return(
             < React.Fragment key={item.title}>
                 <div 
-                className="title active"
+                className= {`title ${active}`}
                 //we cant use onclick={onTitleClick} so it wont envoked the moment the component is rendered
                  onClick = {()=> onTitleClick(index)}
                  >
                     <i className="dropdown icon"></i>
                     {item.title}
                 </div>
-                <div className="content active">
+                <div className={`content ${active}`}>
                     <p>{item.content}</p>
                 </div>
             </React.Fragment>
