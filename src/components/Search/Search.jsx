cba430989e94c530ca3fac7a23a2ab3b79637ed6
Rinat Nadav <ringhost@gmail.com>
2020-07-25 23:36:19 +0300

    add a search component to create a new widget

---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,21 @@
+import React, {useState}from 'react';
+
+const Search = ()=>{
+const [term, setTerm] = useState('')
+console.log(term)
+    return(
+        <div>
+            <div className="ui form">
+                <div className="field">
+                    <lable>Enter Search Term</lable>
+                    <input 
+                    value={term}
+                    onChange={e=>{setTerm(e.target.value)}}
+                    className="input"
+                    />
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Search
