1546d4044c977076705b0d35ef7bb4604b0108b1
Rinat Nadav <ringhost@gmail.com>
2020-07-26 22:59:22 +0300

    added wikipedia request using hooks

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,13 +1,31 @@
-import React, {useState}from 'react';
-
+import React, {useState, useEffect}from 'react';
+import axios from 'axios';
 const Search = ()=>{
 const [term, setTerm] = useState('')
-console.log(term)
+
+useEffect(()=>{
+// (async()=>{
+//     await axios.get(``);
+// })();
+    const search = async ()=>{
+        await axios.get(`https://en.wikipedia.org/w/api.php`,{
+            params:{
+                action:'query',
+                list:'search',
+                origin:'*',
+                format:'json',
+                srsearch:"",
+            }
+        });
+    };
+    search();
+        console.log('i run only once');
+},[term])
     return(
         <div>
             <div className="ui form">
                 <div className="field">
-                    <lable>Enter Search Term</lable>
+                    <label>Enter Search Term</label>
                     <input 
                     value={term}
                     onChange={e=>{setTerm(e.target.value)}}
