d5dcc204c262cf2a4a3e9246ae9ae16c88a3d56c
Rinat Nadav <ringhost@gmail.com>
2020-07-26 23:18:32 +0300

    added renderedResults variable with the api data content

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,26 +1,40 @@
 import React, {useState, useEffect}from 'react';
 import axios from 'axios';
 const Search = ()=>{
-const [term, setTerm] = useState('')
-
+const [term, setTerm] = useState('hello');
+const [results, setResults] = useState([]);
+console.log(results)
 useEffect(()=>{
 // (async()=>{
 //     await axios.get(``);
 // })();
     const search = async ()=>{
-        await axios.get(`https://en.wikipedia.org/w/api.php`,{
+        const { data } = await axios.get(`http://en.wikipedia.org/w/api.php`,{
             params:{
                 action:'query',
                 list:'search',
                 origin:'*',
                 format:'json',
-                srsearch:"",
+                srsearch:term,
             }
         });
+        setResults(data.query.search)
     };
     search();
-        console.log('i run only once');
 },[term])
+
+const renderedResults = results.map((result)=>{
+    return(
+    <div className="item" key={result.pageid}>
+        <div className="content">
+            <div className="header">
+                {result.title}
+            </div>
+            {result.snippet}
+        </div>
+    </div>
+    )
+})
     return(
         <div>
             <div className="ui form">
@@ -32,6 +46,9 @@ useEffect(()=>{
                     className="input"
                     />
                 </div>
+            </div>
+            <div className="ui celled list">
+                {renderedResults}
             </div>
         </div>
     )
