0269eb28100e7dd45eba9377af2928238221dd77
Rinat Nadav <ringhost@gmail.com>
2020-07-26 23:49:13 +0300

    added delay only after ffirst render

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -20,13 +20,19 @@ useEffect(()=>{
         });
         setResults(data.query.search)
     };
-    const timeoutId = setTimeout(()=>{
-        if(term){
-            search();
+    if(term && !results.length){
+        search()
+    } else {
+
+        const timeoutId = setTimeout(()=>{
+            if(term){
+                search();
+            }
+        },1000)
+        return()=>{
+            clearTimeout(timeoutId)
         }
-    },1000)
-    return()=>{
-        clearTimeout(timeoutId)
+    
     }
 
 },[term])
