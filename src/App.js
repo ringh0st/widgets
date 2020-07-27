e64cf0dd6587f447090f7d56df4c99fed62962de
Rinat Nadav <ringhost@gmail.com>
2020-07-28 00:00:30 +0300

    dropdown setSelected created

--- src/App.js
+++ src/App.js
@@ -1,27 +1,45 @@
-import React from 'react';
-import Accordion from './components/Accordion/Accordion';
-import Search from './components/Search/Search';
+import React,{useState} from 'react';
+// import Accordion from './components/Accordion/Accordion';
+// import Search from './components/Search/Search';
 import DropDown from './components/DropDown/DropDown';
-const items = [
+// const items = [
+//   {
+//     title:'What is React?',
+//     content:'React is a front-end javascript framework'
+//   },
+//   {
+//     title:'Why use React?',
+//     content:'React is a favorite JS library among engeneers'
+//   },
+//   {
+//     title:'How do you use React?',
+//     content:'You use react by creating component'
+//   }
+// ]
+const options = [
   {
-    title:'What is React?',
-    content:'React is a front-end javascript framework'
+    label:'The Color Red',
+    value:'red'
   },
   {
-    title:'Why use React?',
-    content:'React is a favorite JS library among engeneers'
+    label:'The Color Green',
+    value:'green'
   },
   {
-    title:'How do you use React?',
-    content:'You use react by creating component'
+    label:'Shade of Blue',
+    value:'blue'
   }
 ]
 export default () =>{
+  const [selected,setSelected] = useState(options[0])
   return (
     <div>
       {/* <Accordion items={items}/> */}
       {/* <Search/> */}
-      <DropDown/>
+      <DropDown 
+      selected={selected} 
+      onSelectedChange={setSelected}
+      options={options}/>
     </div>
   );
 }
--- src/components/DropDown/DropDown.jsx
+++ src/components/DropDown/DropDown.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
 
-const DropDown =()=>{
-
+const DropDown =({options, selected, onSelectedChange})=>{
+    const rendersOptions = options.map((option)=>{
+        return(
+        <div 
+        key={option.value} 
+        className="item"
+        onClick={()=>onSelectedChange(option)}
+        >
+            
+            {option.label}</div>
+        )
+    })
     return(
-        <div>DropDown</div>
+        <div className="ui form">
+            <div className="field">
+                <label className="lable">Select a Color</label>
+                <div className="ui selection dropdown visible active">
+                    <i className="dropdown icon"></i>
+                    <div className="text">{selected.label}</div>
+                    <div className="menu visible transition">
+                    {rendersOptions}
+                    </div>
+                </div>
+            </div>
+        </div>
+        
     )
 }
 export default DropDown;
