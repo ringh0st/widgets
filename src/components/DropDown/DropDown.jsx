c99a368f5a56461f41377bd059aa183836da6750
Rinat Nadav <ringhost@gmail.com>
2020-07-28 23:37:15 +0300

    added condition to className of DropDown conponent

--- src/components/DropDown/DropDown.jsx
+++ src/components/DropDown/DropDown.jsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const DropDown =({options, selected, onSelectedChange})=>{
+    const [open, setOpen] =useState(false);
     const rendersOptions = options.map((option)=>{
+        if(option.value === selected.value){
+            return null;
+        }
         return(
         <div 
         key={option.value} 
@@ -16,10 +20,15 @@ const DropDown =({options, selected, onSelectedChange})=>{
         <div className="ui form">
             <div className="field">
                 <label className="lable">Select a Color</label>
-                <div className="ui selection dropdown visible active">
+                <div 
+                onClick={()=>setOpen(!open)}
+                className={`ui selection dropdown ${open ? 'visible active': ''} `}
+                >
                     <i className="dropdown icon"></i>
                     <div className="text">{selected.label}</div>
-                    <div className="menu visible transition">
+                    <div 
+                    className={`menu ${open ? 'visible transition':''}`}
+                    >
                     {rendersOptions}
                     </div>
                 </div>
